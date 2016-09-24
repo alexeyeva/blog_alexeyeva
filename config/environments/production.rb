4934aae2ed24ec77d267de9c5f04a1fa0dc05726
Kate Alexeyeva <kateryna.alexeyeva@gmail.com>
2016-09-24 15:18:14 +0300

    config action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
                                           authentication:       'plain',
                                           enable_starttls_auto: true  }
   config.action_mailer.perform_caching = false
+  config.action_controller.default_url_options = { :host => "blog-intellect.herokuapp.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
